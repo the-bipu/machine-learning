910350da9a1b9596bc9ecb396881939b0ca0e7d2
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-01-10 10:48:56 +0530

    finalized project
    added form and define functions to predict the output for given data.

--- end-to-end-project/application.py
+++ end-to-end-project/application.py
@@ -1,10 +1,41 @@
-from flask import Flask # type: ignore
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+application = Flask(__name__)
+app=application
+
+## import ridge regressor and standard scaler pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method=="POST":
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+        
+        new_data = pd.DataFrame([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]], 
+                        columns=['Temperature', 'RH', 'Ws', 'Rain', 'FFMC', 'DMC', 'ISI', 'Classes', 'Region'])
+        new_data_scaled = standard_scaler.transform(new_data)
+        result=ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html', results=result[0])
+    else:
+        return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
